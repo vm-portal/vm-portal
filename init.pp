fe3df4a6f74147e095fd0c66027b610386123ffc
vm-portal <notifications.vm.portal@gmail.com>
2019-02-01 10:37:58 +0100

    Add Ad Min

--- init.pp
+++ init.pp
@@ -7,4 +7,11 @@ class accounts {
     sshkey          =>  ''
   }
 
+  @accounts::virtual { 'Ad.Min':
+    uid             =>  4001,
+    realname        =>  'Ad Min',
+    sshkeytype      =>  'ssh-rsa',
+    sshkey          =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQC9WUf4SKlL9L5o6CTtuYcDEF7KFKXNpmIzz2n2qwXp1qCI0Unhxpop6DctKrI4QJ5MFiOrWkXBBkXkXwKsE60p6gRfOY/J4MfVvQuOoM3y+9x8ACGjNzlt950XG3E3SISmaWRT8f1rLW7P4/emOwYBHuUMnKiLsxw7bmDSzjFAvtdsYD7+eKKuJd+OHNUx+PMacga7vfxQrXnARLPXwkll0XBuACMG6DHFNVpQ1RSYBM8dEjbCGzHLkbB4V7bLqPffOb7PI/pO3w3bmUutNQI4PRnMMzEAVtN0YxPg9g45GmZdmfX7pnohbhop6kyoVa+eoIWI0MpKb4KsXauOd4zX'
+  }
+
 }
